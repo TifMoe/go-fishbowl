5b107365583b9c23fdd8e6d6f627e22bc9011105
Tiffany Moeller <tmoellerart@gmail.com>
2021-06-13 10:48:40 +0100

    Sentry integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
 
 import './App.css';
 import HomePage from './pages/Home';
@@ -7,14 +8,16 @@ import GamePage from './pages/Game';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-        <Switch>
-         <Route path="/" component={HomePage} exact/>
-         <Route path="/game/:gameId" component={GamePage}/>
-       </Switch>
-    </div>
-  </BrowserRouter>
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/game/:gameId" component={GamePage}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Sentry.ErrorBoundary>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+// import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  // autoSessionTracking: true,
+  // integrations: [
+  //   new Integrations.BrowserTracing(),
+  // ],
+
+  // // We recommend adjusting this value in production, or using tracesSampler
+  // // for finer control
+  // tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
